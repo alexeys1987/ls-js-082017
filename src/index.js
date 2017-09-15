a6ce5b7ce5eec89ae29c109ace4be90b78bdbaec
Alexey Slyusarenko <alexeys1987@gmail.com>
2017-09-15 21:20:11 +0300

    First commit
    Without tasks with a star

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
+
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
+ */
+function delayPromise(seconds) {
+    return new Promise(function(resolved, resolve) {
+        setTimeout(function() {
+            resolved();
+        }, seconds * 1000);
+    });
+
+}
+
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
+ */
+function loadAndSortTowns() {
+    return new Promise(function(resolve) {
+        let xhr = new XMLHttpRequest();
+        // let  sortArray = [];
+
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.responseType = 'json';
+        xhr.addEventListener('load', function() {
+            if (Array.isArray(xhr.response)) {
+                resolve(xhr.response.sort(function(a, b) {
+                    if (a.name > b.name) {
+                        return 1
+                    }
+                    if (a.name < b.name) {
+                        return -1
+                    }
+                }));
+            }
+        });
+        xhr.send();
+    });
+}
+
+export {
+    delayPromise,
+    loadAndSortTowns
+};
